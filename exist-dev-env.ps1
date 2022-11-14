ab8d331612143cb99338e07067c60f8304702b05
Omar Siam <Omar.Siam@oeaw.ac.at>
2022-11-14 18:22:39 +0100

    feat: test and dev container env

---
+++ exist-dev-env.ps1
@@ -0,0 +1,10 @@
+iex "docker run --rm -it -p 48080:8080 ```
+  -v $pwd`:/exist-users-from-env/ ```
+  -v $pwd\logs`:/exist/logs ```
+  -v $(Resolve-Path $pwd\build\existufe*.xar)`:/exist/autodeploy/$(Resolve-Path $pwd\build\existufe*.xar | Split-Path -leaf) ```
+  -e EXIST_admin_password=t3mp ```
+  -e EXIST_user_test1_password=nix ```
+  -e EXIST_user_test2_password=dr1ng3nd ```
+  -e EXIST_group_testgroup=test1 ```
+  -e EXIST_group_testgroup2=test1,test2 ```
+  acdhch/existdb:6.0.1-java11-ShenGC"
